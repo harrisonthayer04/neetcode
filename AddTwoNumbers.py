e52f1974f419aa58ef073e4c5a5ec09483e5cb77
harrisonthayer <harrisonthayer04@gmail.com>
2025-06-12 15:31:30 -0700

    Added one more problem

---
+++ AddTwoNumbers.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def addTwoNumbersHelper(l1, l2, carry):
+            # take care of the base case
+            if not l1 and not l2 and carry == 0:
+                return None
+        
+            v1=l1.val if l1 else 0
+            v2=l2.val if l2 else 0
+
+            total = v1 + v2 + carry
+            new_carry = total // 10
+            node_val = total % 10
+
+            node = ListNode(node_val)
+            node.next = addTwoNumbersHelper(l1.next if l1 else None, l2.next if l2 else None, new_carry)
+            return node
+        return addTwoNumbersHelper(l1,l2,0)
+
+        
+
+        
