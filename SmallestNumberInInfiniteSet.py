2eb7c5f5321f4ab9235b9399d5ed9c66cd6843bd
harrisonthayer <harrisonthayer04@gmail.com>
2025-09-12 17:50:47 -0700

    Added one more

---
+++ SmallestNumberInInfiniteSet.py
@@ -0,0 +1,26 @@
+import heapq
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.heap = []
+        self.min_number = 1
+
+    def popSmallest(self) -> int:
+        if self.heap:
+            return heapq.heappop(self.heap)
+        self.min_number += 1
+        return self.min_number - 1
+
+
+    def addBack(self, num: int) -> None:
+        if self.min_number > num and num not in self.heap:
+            heapq.heappush(self.heap, num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
+
+# Obviously we don't actually want store 
